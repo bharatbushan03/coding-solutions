db449c14486285f462c33cf814c9ccd42f40ca73
Bharat Bushan Lashotra <bharatbushan5320@gmail.com>
2026-07-20 23:01:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int findEquilibrium(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> prefix(n), suffix(n);
+        
+        prefix[0] = arr[0];
+        suffix[n-1] = arr[n-1];
+        
+        for (int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1]+arr[i];
+        }
+        
+        for (int i=n-2; i>=0; i--) {
+            suffix[i] = suffix[i+1] + arr[i];
+        }
+        
+        for (int i=0; i<n; i++) {
+            if (prefix[i] == suffix[i]) return i;
+        }
+        
+        return -1;
+        
+    }
+};
