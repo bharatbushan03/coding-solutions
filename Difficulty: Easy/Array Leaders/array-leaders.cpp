6f9147d7992661dcbf9ba6cbc255340453cfc215
Bharat Bushan Lashotra <bharatbushan5320@gmail.com>
2026-07-20 23:28:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        vector<int> ans;
+        int n = arr.size();
+        ans.push_back(arr[n-1]);
+        
+        for (int i=n-2; i>=0; i--) {
+            if (ans.back() <= arr[i]) ans.push_back(arr[i]);
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
