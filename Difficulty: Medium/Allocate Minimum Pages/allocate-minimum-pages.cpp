98288b2fd5cf4d0d1d3c9fa3dedbb5eeb26337d7
Bharat Bushan Lashotra <bharatbushan5320@gmail.com>
2026-09-12 21:29:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,53 +1,31 @@
 class Solution {
   public:
-    bool check(vector<int> &arr, int k, long long pageLimit) {
-
-    // starting from the first student
-    int cnt = 1;
-    long long pageSum = 0;
-
-    for(int i = 0; i < arr.size(); i++) {
-
-        // if adding the current book exceeds the page
-        // limit, assign the book to the next student
-        if(pageSum + arr[i] > pageLimit) {
-            cnt++;
-            pageSum = arr[i];
+    bool isPossible(vector<int>& arr, long long k, long long mid) {
+        long long pgs = 0, students = 1;
+        for (int i : arr) {
+            if (pgs + i <= mid) pgs += i;
+            else pgs = i, students++;
         }
-        else {
-            pageSum += arr[i];
-        }
+        
+        return students <= k;
     }
-
-    // if books can assigned to less than k students then
-    // it can be assigned to exactly k students as well
-    return (cnt <= k);
-}
-
-int findPages(vector<int> &arr, int k) {
-
-    // if number of students are more than total books
-    // then allocation is not possible
-    if(k > arr.size())
-        return -1;
-
-    // search space for Binary Search
-    long long lo = *max_element(arr.begin(), arr.end());
-    long long hi = accumulate(arr.begin(), arr.end(), 0LL);
-    int res = -1;
-
-    while(lo <= hi) {
-        long long mid = lo + (hi - lo) / 2;
-
-        if(check(arr, k, mid)) {
-            res = (int)mid;
-            hi = mid - 1;
+    
+    int findPages(vector<int> &arr, int k) {
+        int n = arr.size();
+        if (k > n) return -1;
+        int ans = -1;
+        
+        long long st = *max_element(arr.begin(), arr.end()), end = accumulate(arr.begin(), arr.end(), 0LL);
+        
+        while (st <= end) {
+            long long mid = st + (end - st)/2;
+            if (isPossible(arr, (long long)k, mid)) {
+                ans = (int)mid;
+                end = mid - 1;
+            }
+            else st = mid + 1;
         }
-        else {
-            lo = mid + 1;
-        }
+        
+        return ans;
     }
-
-    return res;
-}
 };
