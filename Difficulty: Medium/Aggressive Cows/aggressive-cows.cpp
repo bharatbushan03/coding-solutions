fa2378fb673a560718e83e1f4d8190cbff432a32
Bharat Bushan Lashotra <bharatbushan5320@gmail.com>
2026-09-12 20:51:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,34 +1,33 @@
-
-// User function Template for C++
-
 class Solution {
-  public:
-    bool isValid(vector<int> arr,int n,int mid,int k) {
-        int cow=1,lp=arr[0];
+  private:
+    bool isPossible(vector<int>& arr, int n, int mid, int k) {
+        int cows=1, last = arr[0];
         for (int i=1; i<n; i++) {
-            if (arr[i]-lp>=mid) {
-                cow++;
-                lp=arr[i];
+            if (arr[i] - last >= mid) {
+                cows++;
+                last = arr[i];
             }
-            if (cow==k) return true;
+            if (cows == k) return true;
         }
         
         return false;
     }
+  public:
     int aggressiveCows(vector<int> &arr, int k) {
-        int n=arr.size();
-        if (k>n) return -1;
+        int n = arr.size();
         sort(arr.begin(), arr.end());
-        int st=1, end=arr[n-1]-arr[0];
-        int ans=-1;
-        while (st<=end) {
-            int mid=st+(end-st)/2;
-            if (isValid(arr,n,mid,k)) {
-                ans=mid;
-                st=mid+1;
+        int ans = -1;
+        
+        int st=1, end = *max_element(arr.begin(), arr.end()) - *min_element(arr.begin(), arr.end());
+        
+        while (st <= end) {
+            int mid = st + (end - st)/2;
+            if (isPossible(arr, n, mid, k)) {
+                ans = mid;
+                st = mid + 1;
             }
             else {
-                end=mid-1;
+                end = mid - 1;
             }
         }
         
